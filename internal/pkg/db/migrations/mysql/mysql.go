c9698f1675e0c7114c79413ccf3aa615aa81c6ee
= <sharukhprogramming@gmail.com>
2025-06-08 11:31:32 +0530

    added database migrations

---
+++ internal/pkg/db/migrations/mysql/mysql.go
@@ -0,0 +1,47 @@
+package database
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/golang-migrate/migrate"
+	"github.com/golang-migrate/migrate/database/mysql"
+	_ "github.com/golang-migrate/migrate/source/file"
+	"github.com/golang-migrate/migrate/v4/database/mysql"
+)
+
+var Db *sql.DB 
+
+func InitDB(){
+	db,err := sql.Open("mysql","root:root@tcp(localhost)/hackernews")
+	if err != nil {
+		log.Panic(err.Error())
+	}
+
+	if err = db.Ping(); err != nil {
+		log.Panic(err.Error())
+	}
+	Db = db 
+}
+
+func CloseDB() error {
+	return Db.Close()
+}
+
+func Migrate(){
+	if err := Db.Ping(); err != nil {
+		log.Fatal(err.Error())
+	}
+	driver,_ := mysql.WithInstance(Db,&mysql.Config{})
+
+	m,_ := migrate.NewWithDatabaseInstance(
+		"file://internal/pkg/db/migrations/mysql",
+		"mysql",
+		driver,
+	)
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange{
+		log.Fatal(err)
+	}
+	
+}
